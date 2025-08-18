4b25c3e7fc855eda6c8a274d9893b9dcf54c0a6b
Farhan <anak.upload90@gmail.com>
2025-08-18 20:27:49 +0800

    feat: setup routes

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,11 +1,11 @@
 import { ScrollView, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function TabTwoScreen() {
+export default function LoginScreen() {
   return (
     <SafeAreaView style={style.container}>
       <ScrollView>
-        <Text>Hello World: Explore</Text>
+        <Text>Hello World: Login</Text>
       </ScrollView>
     </SafeAreaView>
   );
